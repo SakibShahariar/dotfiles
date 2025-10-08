f57aea57519e2f060276726a8937b190543fe44f
Sakib <sakib209870@gmail.com>
2025-10-08 12:22:48 +0600

    removed space from duplicate file name

--- Scripts/Scripts/duplicate.sh
+++ Scripts/Scripts/duplicate.sh
@@ -4,7 +4,7 @@
 for f in "$@"; do
   dir="$(dirname "$f")"
   base="$(basename "$f")"
-  
+
   # Determine base name and extension more robustly
   if [[ "$base" == *.* && "${base%.*}" != "" ]]; then
     ext=".${base##*.}"
@@ -17,14 +17,14 @@ for f in "$@"; do
   # Find a unique filename for the copy
   counter=1
   # First, try creating "(copy)"
-  dest="$dir/$name (copy)$ext"
+  dest="$dir/$name(copy)$ext"
 
   # If it exists, start trying "(copy 2)", "(copy 3)", etc.
   while [ -e "$dest" ]; do
     counter=$((counter + 1))
     dest="$dir/$name (copy $counter)$ext"
   done
-  
+
   echo "Copying '$f' to '$dest'"
   cp -r "$f" "$dest"
 done
